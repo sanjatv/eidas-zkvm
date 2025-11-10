9838bd67641277859b037f62e5f970ef1d3298b7
Sanjat Vangaeva <sanjatvangaeva@Sanjats-MacBook-Pro.local>
2025-11-11 00:56:10 +0100

    guest program for jwt validation

--- methods/guests/jwt_validator/src/main.rs
+++ methods/guests/jwt_validator/src/main.rs
@@ -1,4 +1,6 @@
 use risc0_zkvm::guest::env;
+use jwt_compact::alg::Es256;
+use jwt_compact::prelude::*;
 
 fn main() {
     // TODO: Implement your guest code here
@@ -6,8 +8,20 @@ fn main() {
     // read the input
     let input: u32 = env::read();
 
+    let jwk_json: &str =
+        r#"
+        {
+            "kty": "EC",
+            "crv": "P-256",
+            "x": "e71HaZVzVFi1ypluxqzvdrgfWyZ0NSwFYE_0vppUt6I",
+            "y": "YYvY62S0wOSxejfYeTLB6_3Y9J1Xr2PnZ94VUVJsrQc"
+        }
+    "#;
+
+    let key = Es256.verifying_key_from_jwk(jwk_json)?;
     // TODO: do something with the input
 
     // write public output to the journal
+
     env::commit(&input);
 }
