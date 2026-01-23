503e8fe1aae006a7e4b824feb28ad7921f660f5c
sanjatv <vangisanjat3@gmail.com>
2026-01-23 14:57:37 +0100

    separate verification and zkp generation and use verify_age_over_18 function in host main

--- host/src/api.rs
+++ host/src/api.rs
@@ -1,4 +1,5 @@
-use host::{ verify_age_over_18 };
+use host::{ create_zkp_age_over_18, verify_age_over_18 };
+use risc0_zkvm::Receipt;
 use serde::{ Deserialize, Serialize };
 use axum::{ Router, extract::Json, http::{ self, Method }, routing::post };
 use tower_http::cors::{ Any, CorsLayer };
@@ -15,15 +16,22 @@ pub struct ApiVerifyResponse {
 }
 
 async fn http_verify_age_over_18(Json(req): Json<ApiVerifyRequest>) -> Json<ApiVerifyResponse> {
-    let (is_valid, _) = verify_age_over_18(&req.jwt).unwrap();
+    let receipt: Receipt = create_zkp_age_over_18(&req.jwt).unwrap();
+    let (is_valid, _) = verify_age_over_18(receipt).unwrap();
     Json(ApiVerifyResponse { is_valid })
 }
 
 pub async fn start_server() {
+    // CORS configuration
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<http::HeaderValue>().unwrap())
+        .allow_methods([Method::POST])
+        .allow_headers(Any);
     // Create a new route 'verify' (post) to the router
     // f.eks. kaller POST http://localhost ../verify kommer til app
     // post(verify_http): denne ruten svarer på post requests. når en post request kommer på /verify, kall verify_http
-    let app = Router::new().route("/verify", post(http_verify_age_over_18));
+    //let app = Router::new().route("/verify", post(http_verify_age_over_18));
+    let app = Router::new().route("/verify", post(http_verify_age_over_18)).layer(cors);
 
     // lag TCP server som lytter på en nettverksadresse
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3030").await.unwrap();
