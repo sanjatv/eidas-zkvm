69207203d07176631e5184811c73f067e9536fa0
linahauck <lina.kantega@kantega.no>
2026-01-27 11:22:06 +0100

    senderbase64 encoded receipt til wallet
    inkluderer cors i commiten etter avklaring paa veiledermote

--- host/src/main.rs
+++ host/src/main.rs
@@ -7,37 +7,37 @@ use risc0_zkvm::Receipt;
 // inside modules enums or crates.
 mod api; // This makes host/src/api.rs available
 
-fn main() -> Result<()> {
-    // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
-    tracing_subscriber
-        ::fmt()
-        .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
-        .init();
+// fn main() -> Result<()> {
+//     // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
+//     tracing_subscriber
+//         ::fmt()
+//         .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
+//         .init();
 
-    let filepath_jwt_encoded: &str = "PIDVCencoded";
-    let jwt = fs::read_to_string(filepath_jwt_encoded).expect("Not able to read file");
+//     let filepath_jwt_encoded: &str = "PIDVCencoded";
+//     let jwt = fs::read_to_string(filepath_jwt_encoded).expect("Not able to read file");
 
-    let receipt: Receipt = create_zkp_age_over_18(jwt.as_str())?;
-    println!("receipt: {}", b64_encode_receipt(receipt));
+//     let receipt: Receipt = create_zkp_age_over_18(jwt.as_str())?;
+//     println!("receipt: {}", b64_encode_receipt(receipt));
 
-    // use '?' to forward errors to the caller instead of handling errors here. Vet ikke om dette er bedre enn å ha 'match'
-    // let (is_valid, _) = verify_age_over_18(receipt)?;
+//     // use '?' to forward errors to the caller instead of handling errors here. Vet ikke om dette er bedre enn å ha 'match'
+//     // let (is_valid, _) = verify_age_over_18(receipt)?;
 
-    // println!("journal: {is_valid}");
+//     // println!("journal: {is_valid}");
 
-    // The receipt was verified at the end of proving, but the below code is an
-    // example of how someone else could verify this receipt.
-    //receipt.verify(AGE_OVER_18_ID).unwrap();
+//     // The receipt was verified at the end of proving, but the below code is an
+//     // example of how someone else could verify this receipt.
+//     //receipt.verify(AGE_OVER_18_ID).unwrap();
 
-    Ok(())
-}
+//     Ok(())
+// }
 
 // ------- HTTP entry point -------
 
 // Må ha med #[tokio::main] fordi fordi start_server() er async
-// #[tokio::main]
-// async fn main() -> anyhow::Result<()> {
-//     // Start the HTTP server (this never returns until you stop it)
-//     api::start_server().await;
-//     Ok(())
-// }
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Start the HTTP server (this never returns until you stop it)
+    api::start_server().await;
+    Ok(())
+}
